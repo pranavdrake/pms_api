c00001548db92063086fc508262c3ea7244ef76b
pranavdrake <sharmaps112000@gmail.com>
2023-01-09 15:20:23 +0530

    added importing func

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,83 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import pandas as pd
+from .models import *
+from django.apps import apps
 
-# Create your views here.
+@api_view(['GET'])
+def import_room_types(request):
+    file  = 'mediafiles/import_data/all_room_types.csv'
+    df = pd.read_csv(file)
+    df = df.drop_duplicates(subset=['Type Name'])
+
+    for index, row in df.iterrows():
+        property = Property.objects.first()
+        room_type, created = RoomType.objects.update_or_create(
+            property=property,
+            room_type=row['Type Name'],
+            defaults={
+                'max_adults': row['Number Of Adults'],
+                'max_children': row['Number Of Child'],
+                'total_number_of_rooms': row['Total Number of Rooms'],
+            }
+        )
+    return Response({'room types imported':'room types imported'})
+
+@api_view(['GET'])
+def import_rooms(request):
+    file  = 'mediafiles/import_data/all_rooms.csv'
+    df = pd.read_csv(file)
+    df = df.drop_duplicates(subset=['Room Number'])
+
+
+    for index, row in df.iterrows():
+
+        if Floor.objects.filter(property = Property.objects.first(), floor=int(row['Floor'])).exists():
+            floor = Floor.objects.get(property = Property.objects.first(), floor=int(row['Floor']))
+        else:
+            Floor.objects.create(property = Property.objects.first(), floor=int(row['Floor']))
+            floor = Floor.objects.get(property = Property.objects.first(), floor=int(row['Floor']))
+
+        room_type = RoomType.objects.get(room_type=row['Room Type'])
+        room, created = Room.objects.update_or_create(
+            floor=floor,
+            room_type=room_type,
+            room_number=row['Room Number'],
+            defaults={
+                'room_status': row['Room Status'],
+                'front_office_status': row['FO Status'],
+                'reservation_status': row['Reservation Status'],
+            }
+        )
+
+    return Response({'rooms imported':'rooms imported'})
+
+@api_view(['GET'])
+def import_market_groups(request):
+    file  = 'mediafiles/import_data/all_market_groups.csv'
+    df = pd.read_csv(file)
+    df = df.drop_duplicates(subset=['Market Group'])
+    for index, row in df.iterrows():
+        if row['Status'].strip()=='Active':
+            is_active = True
+
+        market_group, created = MarketGroup.objects.update_or_create(market_group=row['Market Group'], defaults={'description': row['Description'], 'is_active': is_active})
+    
+    return Response({'market groups imported':'market groups imported'})
+
+@api_view(['GET'])
+def import_market_codes(request):
+    file  = 'mediafiles/import_data/all_market_codes.csv'
+    df = pd.read_csv(file)
+    df = df.drop_duplicates(subset=['Market Code'])
+
+    for index, row in df.iterrows():
+        if row['Status'].strip()=='Active':
+            is_active = True
+
+        market_group=MarketGroup.objects.get(market_group = row['Market Group'].strip())
+        market_code, created = MarketCode.objects.update_or_create(market_group=market_group, market_code = row['Market Code'], defaults={'description': row['Description'], 'is_active': is_active})
+
+    return Response({'market codes imported':'market codes imported'})
+
