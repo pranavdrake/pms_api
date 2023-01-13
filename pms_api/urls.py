6dc320a19597944cb3628dda49afec149b51aa8d
sreenivasan2078 <sreenivasan2078@gmail.com>
2023-01-13 19:17:49 +0530

    Added import code

--- pms_api/urls.py
+++ pms_api/urls.py
@@ -24,5 +24,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reservations/', include('reservations.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
